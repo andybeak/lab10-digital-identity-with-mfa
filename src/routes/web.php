a497cd8e03b3fefa74645f40d4945c8db370cc48
andybeak <andy.beak@protonmail.ch>
2020-05-26 11:57:36 +0100

    Move the validate of 2FA out of auth middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,8 +22,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::middleware(['web', 'auth'])->group(function () {
 
     Route::get('/2fa/enroll', 'TwoFactorController@enableTwoFactor');
-    Route::get('/2fa/disable', 'TwoFactorController@disableTwoFactor');
-    Route::get('/2fa/validate', 'Auth\LoginController@getValidateToken');
-    Route::post('/2fa/validate', ['middleware' => 'throttle:5', 'uses' => 'Auth\LoginController@postValidateToken']);
+    Route::get('/2fa/disable', 'TwoFactorController@disableTwoFactor');    
 
 });
+
+Route::get('/2fa/validate', 'Auth\LoginController@getValidateToken');
+Route::post('/2fa/validate', ['middleware' => 'throttle:5', 'uses' => 'Auth\LoginController@postValidateToken']);
